716f1373436246fbc154360c84a1030004fc5f7c
Tad Fisher <tadfisher@gmail.com>
2020-12-09 17:44:20 -0800

    flake: init

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Wrap Gradle builds with Nix";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      rec {
+        packages.gradle2nix = import ./default.nix { inherit pkgs; };
+        defaultPackage = packages.gradle2nix;
+
+        apps.gradle2nix = {
+          type = "app";
+          program = "${packages.gradle2nix}/bin/gradle2nix";
+        };
+        defaultApp = apps.gradle2nix;
+      }
+    );
+}
